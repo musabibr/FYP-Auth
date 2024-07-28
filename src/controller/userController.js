6b50ba11088df795aa54363ad8383893dea1f37c
musabibr <103640081+musabibr@users.noreply.github.com>
2024-07-28 15:11:02 +0200

    Update userController.js
    bug

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -379,7 +379,7 @@ const createSendToken = async (res, req, user ,otp, isVerified,message) => {
         httpOnly: true,
         maxAge: 30 * 24 * 60 * 60 * 1000,
         secure:false// secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
-sameSite:none
+sameSite:'none',
     });
     req.user = newUser;
     res.locals.user = newUser;
